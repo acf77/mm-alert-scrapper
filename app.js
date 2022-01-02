94cc797caa5b4737ff9b2d906488af4ae91abf1a
acf77 <antoniocarlos.j77@gmail.com>
2022-01-02 14:52:02 -0300

    added Ian Lima edit, initial page and scrapping complete

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const urlEncodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set("view engine", "ejs");
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => res.render("index"));
 
@@ -14,54 +14,57 @@ app.post("/scrape", urlEncodedParser, (req, res) => {
   const alertLink = req.body.alertLink;
   console.log(alertLink);
 
-  async function scrape() {
-    const browser = await puppeteer.launch({});
-    const page = await browser.newPage();
+  if (alertLink !== "" || undefined) {
+    async function scrape() {
+      const browser = await puppeteer.launch({});
+      const page = await browser.newPage();
 
-    await page.goto(alertLink);
-    const merchantId = await page.waitForSelector(
-      "#mdi-header > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > a"
-    );
-    const merchantName = await page.waitForSelector(
-      "body > div > div > sre-default-navigation > div:nth-child(2) > div > div > div.ng-scope > div > div > div.oneglance > div.merchant > span"
-    );
-    const alertId = await page.waitForSelector(
-      "body > div > div > sre-default-navigation > div:nth-child(2) > div > div > div.ng-scope > div > div > div.oneglance > div.meta > div.alertId.ng-binding"
-    );
+      await page.goto(alertLink);
+      const merchantId = await page.waitForSelector(
+        "#mdi-header > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > a"
+      );
+      const merchantName = await page.waitForSelector(
+        "body > div > div > sre-default-navigation > div:nth-child(2) > div > div > div.ng-scope > div > div > div.oneglance > div.merchant > span"
+      );
+      const alertId = await page.waitForSelector(
+        "body > div > div > sre-default-navigation > div:nth-child(2) > div > div > div.ng-scope > div > div > div.oneglance > div.meta > div.alertId.ng-binding"
+      );
 
-    const merchantIdText = await page.evaluate(
-      (merchantId) => merchantId.textContent,
-      merchantId
-    );
-    const merchantNameText = await page.evaluate(
-      (merchantName) => merchantName.textContent,
-      merchantName
-    );
-    const alertIdText = await page.evaluate(
-      (alertId) => alertId.textContent,
-      alertId
-    );
+      const merchantIdText = await page.evaluate(
+        (merchantId) => merchantId.textContent,
+        merchantId
+      );
+      const merchantNameText = await page.evaluate(
+        (merchantName) => merchantName.textContent,
+        merchantName
+      );
+      const alertIdText = await page.evaluate(
+        (alertId) => alertId.textContent,
+        alertId
+      );
 
-    const merchantIdTextclear = merchantIdText.replace(/\s+/g, " ").trim();
-    const alertIdTextClear = alertIdText.replace("Alert ID:", "").trim();
+      const merchantIdTextclear = merchantIdText.replace(/\s+/g, " ").trim();
+      const alertIdTextClear = alertIdText.replace("Alert ID:", "").trim();
 
+      browser.close();
 
-    browser.close();
+      const alertData = {
+        "Merchant name": merchantNameText,
+        "Merchant ID": merchantIdTextclear,
+        "Alert ID": alertIdTextClear,
+      };
+      console.log(alertData);
 
-    const alertData = {
-      "Merchant name": merchantNameText,
-      "Merchant ID": merchantIdTextclear,
-      "Alert ID": alertIdTextClear
-    };
-    console.log(alertData);
-
-    res.render("result", {
-      merchantId: alertData["Merchant ID"],
-      merchantName: alertData["Merchant name"],
-      alertId: alertData["Alert ID"]
-    });
+      res.render("result", {
+        merchantId: alertData["Merchant ID"],
+        merchantName: alertData["Merchant name"],
+        alertId: alertData["Alert ID"],
+      });
+    }
+    scrape();
+  } else {
+    res.render("index", { errorDiv: "Paste a link to create a template" });
   }
-  scrape();
 });
 
 app.listen(8080, () => console.log("Server started!"));
