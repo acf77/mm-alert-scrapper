4fcc597e970baf47223560f650dde0a4f7b9067e
acf77 <antoniocarlos.j77@gmail.com>
2022-01-03 15:21:30 -0300

    added empty link error message

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 const express = require("express");
 const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
 
 const app = express();
 const urlEncodedParser = bodyParser.urlencoded({ extended: false });
@@ -10,61 +11,69 @@ app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => res.render("index"));
 
-app.post("/scrape", urlEncodedParser, (req, res) => {
-  const alertLink = req.body.alertLink;
-  console.log(alertLink);
+app.post("/scrape", urlEncodedParser,
+  [
+    check("alertLink", "You must add an alert link!")
+      .exists()
+      .isLength({ min: 1 }),
+  ], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorArray = errors.array();
+      res.render('index-error', {errorArray: errorArray[0].msg});
+    } else {
+      const alertLink = req.body.alertLink;
 
-  if (alertLink !== "" || undefined) {
-    async function scrape() {
-      const browser = await puppeteer.launch({});
-      const page = await browser.newPage();
+      async function scrape() {
+        const browser = await puppeteer.launch({});
+        const page = await browser.newPage();
 
-      await page.goto(alertLink);
-      const merchantId = await page.waitForSelector(
-        "#mdi-header > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > a"
-      );
-      const merchantName = await page.waitForSelector(
-        "body > div > div > sre-default-navigation > div:nth-child(2) > div > div > div.ng-scope > div > div > div.oneglance > div.merchant > span"
-      );
-      const alertId = await page.waitForSelector(
-        "body > div > div > sre-default-navigation > div:nth-child(2) > div > div > div.ng-scope > div > div > div.oneglance > div.meta > div.alertId.ng-binding"
-      );
+        await page.goto(alertLink);
+        const merchantId = await page.waitForSelector(
+          "#mdi-header > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > a"
+        );
+        const merchantName = await page.waitForSelector(
+          "body > div > div > sre-default-navigation > div:nth-child(2) > div > div > div.ng-scope > div > div > div.oneglance > div.merchant > span"
+        );
+        const alertId = await page.waitForSelector(
+          "body > div > div > sre-default-navigation > div:nth-child(2) > div > div > div.ng-scope > div > div > div.oneglance > div.meta > div.alertId.ng-binding"
+        );
 
-      const merchantIdText = await page.evaluate(
-        (merchantId) => merchantId.textContent,
-        merchantId
-      );
-      const merchantNameText = await page.evaluate(
-        (merchantName) => merchantName.textContent,
-        merchantName
-      );
-      const alertIdText = await page.evaluate(
-        (alertId) => alertId.textContent,
-        alertId
-      );
+        const merchantIdText = await page.evaluate(
+          (merchantId) => merchantId.textContent,
+          merchantId
+        );
+        const merchantNameText = await page.evaluate(
+          (merchantName) => merchantName.textContent,
+          merchantName
+        );
+        const alertIdText = await page.evaluate(
+          (alertId) => alertId.textContent,
+          alertId
+        );
 
-      const merchantIdTextclear = merchantIdText.replace(/\s+/g, " ").trim();
-      const alertIdTextClear = alertIdText.replace("Alert ID:", "").trim();
+        browser.close();
 
-      browser.close();
+        const merchantIdTextclear = merchantIdText.replace(/\s+/g, " ").trim();
+        const alertIdTextClear = alertIdText.replace("Alert ID:", "").trim();
 
-      const alertData = {
-        "Merchant name": merchantNameText,
-        "Merchant ID": merchantIdTextclear,
-        "Alert ID": alertIdTextClear,
-      };
-      console.log(alertData);
+        const alertData = {
+          "Merchant name": merchantNameText,
+          "Merchant ID": merchantIdTextclear,
+          "Alert ID": alertIdTextClear,
+        };
+        console.log(alertData);
 
-      res.render("result", {
-        merchantId: alertData["Merchant ID"],
-        merchantName: alertData["Merchant name"],
-        alertId: alertData["Alert ID"],
-      });
+        res.render("result", {
+          merchantId: alertData["Merchant ID"],
+          merchantName: alertData["Merchant name"],
+          alertId: alertData["Alert ID"],
+        });
+      }
+
+      scrape();
     }
-    scrape();
-  } else {
-    res.render("index", { errorDiv: "Paste a link to create a template" });
   }
-});
+);
 
 app.listen(process.env.PORT || 8080, () => console.log("Server started!"));
